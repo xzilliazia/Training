7c32ff409b086018c9115778e0cdd05a550eac6e
mohziaulhaq <mziaulhaq05@gmail.com>
2025-02-21 21:37:38 +0700

    second

--- src/Modul01/Codelab1.java
+++ src/Modul01/Codelab1.java
@@ -8,15 +8,22 @@ public class Codelab1 {
         try (Scanner input = new Scanner(System.in)) {
             System.out.print("Nama: ");
             String nama = input.nextLine();
-
+        String gender = "";
+        while(true){
             System.out.print("Masukkan Jenis Kelamin (L/P): ");
-            String gender = input.next();
+            gender = input.next();
             if (gender.equalsIgnoreCase("l")){
                 gender = "Laki-laki";
+                break;
             }
             else if(gender.equalsIgnoreCase("p")){
                 gender = "Perempuan";
+                break;
+            } 
+            else {
+                System.out.println("Pilihann Tidak valid");
             }
+        }
 
             System.out.print("Tahun Lahir : ");
             int timeuser = input.nextInt();
@@ -25,8 +32,8 @@ public class Codelab1 {
             
             System.out.println("\n===DATA ANDA===");
             System.out.println("Nama anda\t: " + nama);
-            System.out.println("Jelis Kelamin\t: " + gender);
-            System.out.println("Umur\t\t:" + ages + " tahun");
+            System.out.println("Jenis Kelamin\t: " + gender);
+            System.out.println("Umur\t\t: " + ages + " tahun");
         }
     }
 }
