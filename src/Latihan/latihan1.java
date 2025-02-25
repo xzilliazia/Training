a2268b9c28e2c09572916e4427b2f23edda1064e
xzilliazia <mziaulhaq05@gmail.com>
2025-02-25 11:11:28 +0700

    add player2

--- src/Latihan/latihan1.java
+++ src/Latihan/latihan1.java
@@ -13,7 +13,14 @@ class  Player{
         this.health = health;
 
     }
-
+    void attacking (Player opponent){
+        double attackPw = this.weapon.attackPw;
+        System.out.println(this.name + " attacking " + opponent.name + " With Power "+ attackPw);
+        opponent.defence(attackPw);
+    }
+    void defence (double attackPw){
+        System.out.println(this.name + " Gots damage "+ attackPw);
+    }
     void equipWeapon(Weapon weapon){
         this.weapon = weapon;
     }
@@ -26,17 +33,18 @@ class  Player{
         this.weapon.display();
         this.armor.display();
     }
+
 }
 //senjata
 class Weapon{
-    double attack;
+    double attackPw;
     String name;
     Weapon(String name, double attack){
-        this.attack = attack;
+        this.attackPw = attack;
         this.name = name;
     }
     void display(){
-        System.out.println("Weapon : " + this.name + " , Attack : " + this.attack);
+        System.out.println("Weapon : " + this.name + " , Attack : " + this.attackPw);
     }
 }
 //armor
@@ -53,23 +61,35 @@ class Armor{
     }
 }
 
-
 public class latihan1 {
     
     public static void main(String[] args) {
         //object player
         Player player1 = new Player("Jono",100);
+        Player player2 = new Player("Danang",80);
         
         //object weapon
         Weapon sword = new Weapon("sword", 15);
+        Weapon Bow = new Weapon("Bow", 30);
 
         //object armor 
         Armor besi = new Armor("Iron", 10);
+        Armor Shirt = new Armor("Leather", 2);
 
         //perlengkapan player 1
         player1.equipWeapon(sword);
         player1.equipArmor(besi);
         player1.dislpay();
+
+        //Player2
+        player2.equipWeapon(Bow);
+        player2.equipArmor(Shirt);
+        player2.dislpay();
+
+        System.out.println("\nPERTEMPURAN");
+        player1.attacking(player2);
+        
+
 
     }
 }
