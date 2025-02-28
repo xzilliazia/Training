f10eb8c9cd73464a655f37cf423760c4eec3b40b
Mohammad Ziaulhaq Alhasbuki <141542406+xzilliazia@users.noreply.github.com>
2025-02-28 21:41:51 +0700

    AccessPrivate

---
+++ src/Latihan/pbcprv.java
@@ -0,0 +1,69 @@
+package Latihan;
+
+class Playerr{
+    String name; //default, dia bisa dibaca dan tulis
+    public int exp; // Public, bisa baca tulis.
+    private int health; //hanya kan bisa dibaca,tulis didalam class saja.
+    
+    //construktor
+    Playerr (String name, int exp, int health){
+        this.name = name;
+        this.exp = exp;
+        this.health = health;
+        
+    }
+
+    //default modifer access, bisa dibaca diluar
+    //membaca didalam class
+    void display(){
+        tambahExp(); // mengakses private methods
+        System.out.println("\nName\t: " + this.name);
+        System.out.println("Exp\t: " + this.exp);
+        System.out.println("Health\t: " + this.health);
+    }
+    public void ubahnama(String namaBaru){ 
+        this.name = namaBaru;
+
+    }
+    //private
+    private void tambahExp(){
+        this.exp += 100;
+    }
+}
+
+public class pbcprv {
+    public static void main(String[] args) {
+
+        Playerr player1 = new Playerr("Satu", 0, 100);
+
+            //default
+            System.out.println(player1.name);//membaca
+            player1.name = "berbeda";//menulis
+            System.out.println(player1.name);//membaca
+
+            //public
+            System.out.println(player1.exp);
+            player1.exp = 100;
+            System.out.println(player1.exp);
+
+            // //Private
+            // System.out.println(player1.health);
+            // player1.health = 100;
+            // System.out.println(player1.health);
+
+            //methods 
+
+            //default
+
+            player1.display();
+
+
+        //public
+        player1.ubahnama("rudi");
+        player1.display();
+
+        //private tidak bisa diakses
+        // player1.tambahExp();
+
+        }
+}
